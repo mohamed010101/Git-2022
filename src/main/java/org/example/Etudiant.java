64ef4480f6df1e96aa239ebabb3489b3ffe3fef6
Ben Youssef Mohamed <mohamed.benyoussef@esprit.tn>
2023-01-23 20:27:41 +0100

    Commit-004

---
+++ src/main/java/org/example/Etudiant.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Etudiant {
+    private int Id;
+    private String Nom;
+    private String Prenom;
+    private int Moyenne;
+}
+
+/*class Etudiant
+{ private String Nom ;
+ protected String adresse;
+ protected String Prenom ;
+ f*/
